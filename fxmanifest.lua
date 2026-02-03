7b0472ddcbda6bd7ef94f55a00cbefa409dd6f4d
Master Mind <48193931+iboss21@users.noreply.github.com>
2026-02-02 18:34:38 -0600

    Update warning message formatting in fxmanifest.lua

--- fxmanifest.lua
+++ fxmanifest.lua
@@ -23,7 +23,7 @@
 ]]
 
 fx_version 'cerulean'
-rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources WILL become incompatible once RedM ships.'
+rdr3_warning 'I acknowledge that this is a prerelease build of RedM, and I am aware my resources *will* become incompatible once RedM ships.'
 game 'rdr3'
 
 -- ═══════════════════════════════════════════════════════════════════════════════
